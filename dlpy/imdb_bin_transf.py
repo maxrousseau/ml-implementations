02da6a800def05f8a6aeb2d215d93d99b782f27f
Maxime Rousseau <maximerousseau08@gmail.com>
2020-12-03 22:10:42 -0500

    iter

--- dlpy/imdb_bin_transf.py
+++ dlpy/imdb_bin_transf.py
@@ -7,7 +7,7 @@ from tensorflow.keras import layers
 # [attention is all you need] https://arxiv.org/abs/1706.03762
 # transformer binary classifer
 # 1. import imdb dataset
-# 2. word2vec embedding
+# 2. get the tokenizer from huggingface
 # 3. multihead attention layer
 # 4. embedding layer?
 # 5. feed foward layer
